40406f6bef28de1428fc9c017a6506e76c6a3f70
Jayesh <jayesh.mail@gmail.com>
2013-09-03 12:16:40 +0530

    added google plus profile

--- mysite.py
+++ mysite.py
@@ -1,19 +1,14 @@
 import os
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
-# @app.route('/test')
-# def test_page():
-#     return render_template('test.html')
-
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
