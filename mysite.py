dc1c76268b1de336dc2bbdd4e13416f750364e3c
Jayesh <jayesh.mail@gmail.com>
2013-09-06 12:42:16 +0530

    added test page

--- mysite.py
+++ mysite.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def index_page():
     return render_template('index.html')
 
+@app.route('/test')
+def test_page():
+    return render_template('test_page.html')
 
 if __name__ == "__main__":
     app.run()
